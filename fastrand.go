e0879c15a5608db3a3c0d0c196f9c5191b78caa3
ZYunH <zyunhjob@163.com>
2021-01-18 21:55:22 +0800

    init

---
+++ fastrand.go
@@ -0,0 +1,96 @@
+package fastrand
+
+import (
+	_ "unsafe"
+)
+
+//go:linkname runtimefastrand runtime.fastrand
+func runtimefastrand() uint32
+
+// Uint32 returns a pseudo-random 32-bit value as a uint32.
+func Uint32() uint32 {
+	return runtimefastrand()
+}
+
+// Uint64 returns a pseudo-random 64-bit value as a uint64.
+func Uint64() uint64 {
+	return (uint64(runtimefastrand()) << 32) | uint64(runtimefastrand())
+}
+
+// Int returns a non-negative pseudo-random int.
+func Int() int {
+	u := uint(Int63())
+	return int(u << 1 >> 1) // clear sign bit if int == int32
+}
+
+// Int31 returns a non-negative pseudo-random 31-bit integer as an int32.
+func Int31() int32 { return int32(Uint32() & (1<<31 - 1)) }
+
+// Int63 returns a non-negative pseudo-random 63-bit integer as an int64.
+func Int63() int64 {
+	return int64(Uint64() & (1<<63 - 1))
+}
+
+// Int63n returns, as an int64, a non-negative pseudo-random number in [0,n).
+// It panics if n <= 0.
+func Int63n(n int64) int64 {
+	if n <= 0 {
+		panic("invalid argument to Int63n")
+	}
+	if n&(n-1) == 0 { // n is power of two, can mask
+		return Int63() & (n - 1)
+	}
+	max := int64((1 << 63) - 1 - (1<<63)%uint64(n))
+	v := Int63()
+	for v > max {
+		v = Int63()
+	}
+	return v % n
+}
+
+// Int31n returns, as an int32, a non-negative pseudo-random number in [0,n).
+// It panics if n <= 0.
+// For implementation details, see:
+// https://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction
+func Int31n(n int32) int32 {
+	if n <= 0 {
+		panic("invalid argument to Int31n")
+	}
+	v := Uint32()
+	prod := uint64(v) * uint64(n)
+	low := uint32(prod)
+	if low < uint32(n) {
+		thresh := uint32(-n) % uint32(n)
+		for low < thresh {
+			v = Uint32()
+			prod = uint64(v) * uint64(n)
+			low = uint32(prod)
+		}
+	}
+	return int32(prod >> 32)
+}
+
+// Intn returns, as an int, a non-negative pseudo-random number in [0,n).
+// It panics if n <= 0.
+func Intn(n int) int {
+	if n <= 0 {
+		panic("invalid argument to Intn")
+	}
+	if n <= 1<<31-1 {
+		return int(Int31n(int32(n)))
+	}
+	return int(Int63n(int64(n)))
+}
+
+// Uint32n returns a pseudo-random number in [0,n).
+//go:nosplit
+func Uint32n(n uint32) uint32 {
+	// This is similar to Uint32() % n, but faster.
+	// See https://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction/
+	return uint32(uint64(Uint32()) * uint64(n) >> 32)
+}
+
+// Uint64n returns a pseudo-random number in [0,n).
+func Uint64n(n uint64) uint64 {
+	return Uint64() % n
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zhangyunhao116/fastrand
+
+go 1.15
