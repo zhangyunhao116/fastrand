19caf85333da602542b3f4956a6a960a6b78a954
zhangyunhao <zhangyunhao116@gmail.com>
2021-10-16 23:13:27 +0800

    feat: support Perm and Shuffle

--- fastrand.go
+++ fastrand.go
@@ -154,3 +154,39 @@ func Read(p []byte) (int, error) {
 
 	return len(p), nil
 }
+
+// Shuffle pseudo-randomizes the order of elements.
+// n is the number of elements. Shuffle panics if n < 0.
+// swap swaps the elements with indexes i and j.
+func Shuffle(n int, swap func(i, j int)) {
+	if n < 0 {
+		panic("invalid argument to Shuffle")
+	}
+	// Fisher-Yates shuffle: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+	// Shuffle really ought not be called with n that doesn't fit in 32 bits.
+	// Not only will it take a very long time, but with 2³¹! possible permutations,
+	// there's no way that any PRNG can have a big enough internal state to
+	// generate even a minuscule percentage of the possible permutations.
+	// Nevertheless, the right API signature accepts an int n, so handle it as best we can.
+	i := n - 1
+	for ; i > 1<<31-1-1; i-- {
+		j := int(Int63n(int64(i + 1)))
+		swap(i, j)
+	}
+	for ; i > 0; i-- {
+		j := int(Int31n(int32(i + 1)))
+		swap(i, j)
+	}
+}
+
+// Perm returns, as a slice of n ints, a pseudo-random permutation of the integers
+// in the half-open interval [0,n).
+func Perm(n int) []int {
+	m := make([]int, n)
+	for i := 1; i < n; i++ {
+		j := Intn(i + 1)
+		m[i] = m[j]
+		m[j] = i
+	}
+	return m
+}
--- fastrand_test.go
+++ fastrand_test.go
@@ -12,8 +12,36 @@ func TestAll(t *testing.T) {
 
 	p := make([]byte, 1000)
 	n, err := Read(p)
-	if n != len(p) || err != nil || p[0]+p[1] == 0 {
+	if n != len(p) || err != nil || (p[0] == 0 && p[1] == 0 && p[2] == 0) {
 		t.Fatal()
+	}
+
+	a := Perm(100)
+	for i := range a {
+		var find bool
+		for _, v := range a {
+			if v == i {
+				find = true
+			}
+		}
+		if !find {
+			t.Fatal()
+		}
+	}
+
+	Shuffle(len(a), func(i, j int) {
+		a[i], a[j] = a[j], a[i]
+	})
+	for i := range a {
+		var find bool
+		for _, v := range a {
+			if v == i {
+				find = true
+			}
+		}
+		if !find {
+			t.Fatal()
+		}
 	}
 }
 
