f5e9e7f48426b4cf5a06b4e44c28bad03c014043
zhangyunhao <zhangyunhao116@gmail.com>
2022-02-21 23:41:38 +0800

    update benchmark

--- benchmark_test.go
+++ benchmark_test.go
@@ -7,11 +7,13 @@ import (
 )
 
 type testFuncs struct {
-	fu32  func() uint32
-	fu64  func() uint64
-	fint  func() int
-	fintn func(_ int) int
-	fread func(_ []byte) (int, error)
+	fu32     func() uint32
+	fu64     func() uint64
+	fint     func() int
+	fintn    func(_ int) int
+	fread    func(_ []byte) (int, error)
+	fperm    func(_ int) []int
+	fshuffle func(_ int, swap func(i, j int))
 }
 
 func init() {
@@ -25,18 +27,22 @@ func init() {
 var (
 	targetfs  testFuncs // benchmark this target
 	stdrandfs = testFuncs{
-		fu32:  rand.Uint32,
-		fu64:  rand.Uint64,
-		fint:  rand.Int,
-		fintn: rand.Intn,
-		fread: rand.Read,
+		fu32:     rand.Uint32,
+		fu64:     rand.Uint64,
+		fint:     rand.Int,
+		fintn:    rand.Intn,
+		fread:    rand.Read,
+		fperm:    rand.Perm,
+		fshuffle: rand.Shuffle,
 	}
 	fastrandfs = testFuncs{
-		fu32:  Uint32,
-		fu64:  Uint64,
-		fint:  Int,
-		fintn: Intn,
-		fread: Read,
+		fu32:     Uint32,
+		fu64:     Uint64,
+		fint:     Int,
+		fintn:    Intn,
+		fread:    Read,
+		fperm:    Perm,
+		fshuffle: Shuffle,
 	}
 )
 
@@ -73,6 +79,21 @@ func benchmarkSingleCore(b *testing.B, fs testFuncs) {
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
 			fs.fread(p)
+		}
+	})
+	b.Run("Perm/1024", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			fs.fperm(1024)
+		}
+	})
+	b.Run("Shuffle/1024", func(b *testing.B) {
+		x := make([]int, 1024)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			fs.fshuffle(1024, func(i, j int) {
+				x[i], x[j] = x[j], x[i]
+			})
 		}
 	})
 }
@@ -121,6 +142,14 @@ func benchmarkMultipleCore(b *testing.B, fs testFuncs) {
 		b.RunParallel(func(pb *testing.PB) {
 			for pb.Next() {
 				fs.fread(p)
+			}
+		})
+	})
+	b.Run("Perm/1024", func(b *testing.B) {
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				fs.fperm(1024)
 			}
 		})
 	})
